8afca7e22a768a56830966734444123aa236e112
Zuzu <bde@hazim.dev>
2024-09-17 17:08:57 -0400

    Reformat

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const { getPlayerId, getPlayerMatches, getMatchInformation} = require("./lib/riot_api/endpoints");
-const { MongoHandle } = require("./lib/mongo");
-const { Match } = require("./lib/riot_api/match");
+const {getPlayerId, getPlayerMatches, getMatchInformation} = require("./lib/riot_api/endpoints");
+const {MongoHandle} = require("./lib/mongo");
+const {Match} = require("./lib/riot_api/match");
 
 (async () => {
     const dbHandle = new MongoHandle();
     await dbHandle.init();
 
     let player = {
-        'name': 'BootyFanatic',
-        'tag': 'na1',
+        "name": "BootyFanatic",
+        "tag": "na1",
     };
-    player['puuid'] = await getPlayerId(player);
+    player["puuid"] = await getPlayerId(player);
 
     const recentMatchIds = await getPlayerMatches(player);
     let c = 1;
-    for(const mid of recentMatchIds) {
+    for (const mid of recentMatchIds) {
         const m = new Match(await getMatchInformation(mid));
         const isRecorded = await dbHandle.isMatchRecorded(m.id);
-        if(!isRecorded) {
+        if (!isRecorded) {
             await dbHandle.write(m);
             console.log(`${c++}: Wrote match ${m.toStringAs(player.puuid)}`);
         } else {
