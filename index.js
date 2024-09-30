717328616813b55b2b59455a4dc399db41659855
Zuzu <bde@hazim.dev>
2024-09-30 16:55:25 -0400

    Added metadata about the status of the api key for monitoring.

--- index.js
+++ index.js
@@ -2,31 +2,43 @@ const {getPlayerId, getPlayerMatches, getMatchInformation} = require("./lib/riot
 const {MongoHandle} = require("./lib/mongo");
 const {Match} = require("./lib/riot_api/match");
 const {initLogging} = require('./lib/logging');
+const {ExpiredApiKeyError} = require('./lib/riot_api/errors');
 
 async function main() {
     const dbHandle = new MongoHandle();
     await dbHandle.init();
 
-    let player = {
-        "name": "BootyFanatic",
-        "tag": "na1",
-    };
-    player["puuid"] = await getPlayerId(player);
+    try {
+        let player = {
+            "name": "BootyFanatic",
+            "tag": "na1",
+        };
+        player["puuid"] = await getPlayerId(player);
 
-    const recentMatchIds = await getPlayerMatches(player);
-    let c = 1;
-    for (const mid of recentMatchIds) {
-        const m = new Match(await getMatchInformation(mid));
-        const isRecorded = await dbHandle.isMatchRecorded(m.id);
-        if (!isRecorded) {
-            await dbHandle.write(m);
-            logger.info(`${c++}: Wrote match ${m.toStringAs(player.puuid)}`);
-        } else {
-            logger.info(`${c++}: Already have match ${m.toStringAs(player.puuid)}`);
+        const recentMatchIds = await getPlayerMatches(player);
+        let c = 1;
+        for (const mid of recentMatchIds) {
+            const m = new Match(await getMatchInformation(mid));
+            const isRecorded = await dbHandle.isMatchRecorded(m.id);
+            const ind = Intl.NumberFormat('en-US', {
+                minimumIntegerDigits: 2
+            }).format(c++);
+            if (!isRecorded) {
+                await dbHandle.write(m);
+                logger.info(`${ind}: Wrote match ${m.toStringAs(player.puuid)}`);
+            } else {
+                logger.info(`${ind}: Already have match ${m.toStringAs(player.puuid)}`);
+            }
         }
+        await dbHandle.setExpiredApiKey(false);
+    } catch (e) {
+        if (e instanceof ExpiredApiKeyError) {
+            await dbHandle.setExpiredApiKey(true);
+        }
+        throw e;
+    } finally {
+        await dbHandle.close();
     }
-
-    await dbHandle.close();
 }
 
 (async () => {
