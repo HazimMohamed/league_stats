35421e0a4b5247985ea6a7e281491040b08c0c2e
Zuzu <bde@hazim.dev>
2024-10-02 21:06:44 -0400

    Updated notifier script to notify the last updated date and also deleted dangerous reset function.

--- utils/notify-api-key.js
+++ utils/notify-api-key.js
@@ -1,19 +1,26 @@
 const {MongoHandle} = require('../lib/mongo');
 const winston = require('winston');
+const flags = require('flags');
+
+flags.defineBoolean('printLastUpdated', true, 'Whether or not to print last modified date');
 
 (async () => {
     const mongo = new MongoHandle();
-    let isApiKeyValid;
+    await mongo.init();
+    winston.add(new winston.transports.Console({
+        format: winston.format.cli()
+    }));
+    flags.parse();
+
     try {
-        await mongo.init();
-        isApiKeyValid = await mongo.isApiKeyValid();
+        if (flags.get('printLastUpdated')) {
+            const lastUpdated = await mongo.getLastUpdated();
+            winston.info(`Riot data last updated: ${lastUpdated.toLocaleString()}`);
+        }
+        if (!await mongo.isApiKeyValid()) {
+            winston.warn('Riot API key expired. No longer scraping their servers.');
+        }
     } finally {
         await mongo.close();
-    }
-    if (!isApiKeyValid) {
-        winston.add(new winston.transports.Console({
-            format: winston.format.cli()
-        }));
-        winston.warn('Riot API key expired. No longer scraping their servers.');
     }
 })();
